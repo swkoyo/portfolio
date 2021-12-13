1e8d282ef0ae4a4784d244f82f10d8f1339c6b70
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-13 12:26:06 +0900

    eslint prettier trailing comma error update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [],
+	imports: [
+		MikroOrmModule.forRoot({
+			entities: ['./dist/entities'],
+			entitiesTs: ['./src/entities'],
+			dbName: 'portfolio.sqlite3',
+			type: 'sqlite'
+		})
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService]
 })
 export class AppModule {}
