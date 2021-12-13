cbc59a8022ae6a77e19d9b37d3bccc4fe998d865
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-13 13:15:53 +0900

    update package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,9 @@
-import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [
-		MikroOrmModule.forRoot({
-			entities: ['./dist/entities'],
-			entitiesTs: ['./src/entities'],
-			dbName: 'portfolio.sqlite3',
-			type: 'sqlite'
-		})
-	],
+	imports: [],
 	controllers: [AppController],
 	providers: [AppService]
 })
