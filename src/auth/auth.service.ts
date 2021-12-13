4a0cc5f5138b5bddfb15c61f17068a5d2baf635d
Brandon Kim <bkim2490@gmail.com>
2021-12-13 22:28:47 +0900

    auth added

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+	constructor(
+		private readonly userService: UserService,
+		private readonly jwtService: JwtService
+	) {}
+
+	async validateUser(email: string, password: string) {
+		const user = await this.userService.findOneByEmail(email);
+
+		if (user && (await user.validatePassword(password))) {
+			return user;
+		}
+
+		return null;
+	}
+
+	async createToken(user: any) {
+		return {
+			access_token: this.jwtService.sign({
+				sub: user.id,
+				email: user.email
+			})
+		};
+	}
+}
--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -2,4 +2,5 @@ import { User } from './user.entity';
 
 export interface IUserRO {
 	user: User;
+	validatePassword: (password: string) => Promise<boolean>;
 }
