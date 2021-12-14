6c7d24bbe6d7022ee00bea46570d04724e98c2a8
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-14 19:52:32 +0900

    Pino logger initialized

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { bufferLogs: true });
 	const configService = app.get<ConfigService>(ConfigService);
+	app.useLogger(app.get<Logger>(Logger));
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
 	await app.listen(configService.get<number>('port'));
 }
