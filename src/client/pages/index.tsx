7a38dfac2a197cd3429867b93d5ef8a4ac6db816
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-16 14:48:13 +0900

    using context for user data

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,47 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import {
-	GetServerSideProps,
-	NextPage,
-	InferGetServerSidePropsType
-} from 'next';
-import { useUser } from '../context/UserContext';
+import React from 'react';
+import { NextPage } from 'next';
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
 
-const Index: NextPage = ({
-	userProps
-}: InferGetServerSidePropsType<GetServerSideProps>) => {
-	const [userData, setUserData] = useState(userProps);
-	const { handleUserData, ...contextRest } = useUser();
-
-	useEffect(() => {
-		if (
-			typeof userProps === 'object' &&
-			Object.keys(userProps).length > 0
-		) {
-			handleUserData(userProps);
-		}
-	}, [handleUserData]);
-
-	useEffect(() => {
-		if (
-			Object.keys(userProps).length <= 0 &&
-			Object.keys(contextRest.userData).length > 0
-		) {
-			setUserData(contextRest.userData);
-		}
-	}, []);
+const Index: NextPage = () => {
+	const { userData } = useContext(UserContext);
 
 	return (
 		<div className='text-3xl font-bold underline'>
 			Welcome to {userData.email}'s portfolio!
 		</div>
 	);
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-	const res = await fetch('http://localhost:3000/api/user');
-	const data = await res.json();
-
-	return { props: { userProps: data } };
 };
 
 export default Index;
