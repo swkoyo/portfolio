2380e938c86ee6455cb6a810fdf08bb0d08ba91e
Brandon Kim <bkim2490@gmail.com>
2021-12-16 15:22:43 +0900

    home page basic layout

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -6,8 +6,13 @@ const Index: NextPage = () => {
 	const { userData } = useUserContext();
 
 	return (
-		<div className='text-3xl font-bold underline'>
-			Welcome to {userData.email}'s portfolio!
+		<div className='container mx-auto'>
+			<div className='text-center text-8xl'>
+				{userData.first_name.toUpperCase()}{' '}
+				{userData.last_name.toUpperCase()}
+			</div>
+			<div className='text-center text-2xl'>{userData.tagline}</div>
+			<div className='mt-8 text-center'>{userData.profile}</div>
 		</div>
 	);
 };
