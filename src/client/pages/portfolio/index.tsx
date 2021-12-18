9a44df8a01bcf257669dc58ac1439277c4480700
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-18 12:01:45 +0900

    technology carousel init

--- src/client/pages/portfolio/index.tsx
+++ src/client/pages/portfolio/index.tsx
@@ -3,11 +3,24 @@ import { NextPage } from 'next';
 import { usePortfolioContext } from '../../context/PortfolioContext';
 import ProjectCard from '../../components/ProjectCard';
 
+const TechCarousel = ({ technologies }) => {
+	return (
+		<div className='w-full p-4 space-x-4 carousel carousel-center bg-neutral rounded-box'>
+			{technologies.map((tech, i) => (
+				<div key={i} className='carousel-item'>
+					<div>{tech.name}</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
 const Portfolio: NextPage = () => {
-	const { projectsData } = usePortfolioContext();
+	const { projectsData, technologiesData } = usePortfolioContext();
 
 	return (
 		<div className='container mx-auto'>
+			<TechCarousel technologies={technologiesData} />
 			<div className='grid grid-cols-3 gap-4'>
 				{projectsData.map((project, i) => (
 					<ProjectCard key={i} project={project} />
