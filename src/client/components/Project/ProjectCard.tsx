7220d8e26c1e8e7b9563f87207f757230cfd366e
Brandon Kim <bkim2490@gmail.com>
2021-12-21 12:45:13 +0900

    refactor again

--- src/client/components/Project/ProjectCard.tsx
+++ src/client/components/Project/ProjectCard.tsx
@@ -2,40 +2,22 @@ import { ComponentType } from 'react';
 import { Project } from '../../models';
 import Link from 'next/link';
 import { useAuthContext } from '../../context/AuthContext';
-import cookieCutter from 'cookie-cutter';
 import { usePortfolioContext } from '../../context/PortfolioContext';
 
 interface Props {
 	project: Project;
 }
 
-const deleteTechnology = async (name: string) => {
-	await fetch(
-		`http://localhost:3000/api/projects?name=${encodeURIComponent(name)}`,
-		{
-			method: 'DELETE',
-			headers: {
-				authorization: `Bearer ${cookieCutter.get('token')}`
-			}
-		}
-	);
-};
-
 const ProjectCard: ComponentType<Props> = ({ project }) => {
 	const { auth } = useAuthContext();
-	const { refreshData } = usePortfolioContext();
-
-	const handleDeleteProject = async (name: string) => {
-		await deleteTechnology(name);
-		await refreshData();
-	};
+	const { deleteProject } = usePortfolioContext();
 
 	return (
 		<div className='card shadow bg-blue-300 relative'>
 			{auth ? (
 				<div
 					className='absolute top-0 right-0 btn btn-circle btn-xs btn-error'
-					onClick={() => handleDeleteProject(project.name)}
+					onClick={() => deleteProject(project.name)}
 				>
 					X
 				</div>
