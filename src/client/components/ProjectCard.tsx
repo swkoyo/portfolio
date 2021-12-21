888e317d20655e10bd03ce35203532259d62bcdb
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-21 12:00:43 +0900

    delete project

--- src/client/components/ProjectCard.tsx
+++ src/client/components/ProjectCard.tsx
@@ -1,21 +1,52 @@
 import { ComponentType } from 'react';
 import { Project } from '../models';
 import Link from 'next/link';
+import { useAuthContext } from '../context/AuthContext';
+import cookieCutter from 'cookie-cutter';
+import { usePortfolioContext } from '../context/PortfolioContext';
 
 interface Props {
 	project: Project;
 }
 
+const deleteTechnology = async (name: string) => {
+	await fetch(
+		`http://localhost:3000/api/projects?name=${encodeURIComponent(name)}`,
+		{
+			method: 'DELETE',
+			headers: {
+				authorization: `Bearer ${cookieCutter.get('token')}`
+			}
+		}
+	);
+};
+
 const ProjectCard: ComponentType<Props> = ({ project }) => {
+	const { auth } = useAuthContext();
+	const { refreshData } = usePortfolioContext();
+
+	const handleDeleteProject = async (name: string) => {
+		await deleteTechnology(name);
+		await refreshData();
+	};
+
 	return (
-		<Link href={`/portfolio/${project.name}`}>
-			<div className='card shadow bg-blue-300 hover:bg-black hover:cursor-pointer'>
-				<div className='card-body'>
+		<div className='card shadow bg-blue-300 relative'>
+			{auth ? (
+				<div
+					className='absolute top-0 right-0 btn btn-circle btn-xs btn-error'
+					onClick={() => handleDeleteProject(project.name)}
+				>
+					X
+				</div>
+			) : null}
+			<Link href={`/portfolio/${project.name}`}>
+				<div className='card-body hover:bg-black hover:cursor-pointer'>
 					<h2 className='card-title'>{project.name}</h2>
 					<p>{project.description}</p>
 				</div>
-			</div>
-		</Link>
+			</Link>
+		</div>
 	);
 };
 
