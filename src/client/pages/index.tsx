3bb439ad056598c400198f44ee4621ec3bd7f17d
Brandon Kim <bkim2490@gmail.com>
2021-12-21 12:10:15 +0900

    main profile update

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -6,13 +6,10 @@ const Index: NextPage = () => {
 	const { userData } = useUserContext();
 
 	return (
-		<div className='container mx-auto'>
-			<div className='text-center text-8xl'>
-				{userData.first_name.toUpperCase()}{' '}
-				{userData.last_name.toUpperCase()}
-			</div>
-			<div className='text-center text-2xl'>{userData.tagline}</div>
-			<div className='mt-8 text-center'>{userData.profile}</div>
+		<div className='container mx-auto space-y-14 text-center'>
+			<div className='text-8xl'>{userData.full_name.toUpperCase()}</div>
+			<div className='text-2xl'>{userData.tagline}</div>
+			<div>{userData.profile}</div>
 		</div>
 	);
 };
