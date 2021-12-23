c3516caddb31ccf5dd6e5330bdc4acfc6cd2ed39
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-23 20:27:38 +0900

    projects page styling

--- src/client/pages/portfolio/index.tsx
+++ src/client/pages/portfolio/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NextPage } from 'next';
-import ProjectsGrid from '../../components/Project/ProjectsItems';
+import ProjectsItems from '../../components/Project/ProjectsItems';
 import TechnologyGrid from '../../components/Technology/TechnologyGrid';
 
 const Portfolio: NextPage = () => {
 	return (
-		<div className='grid grid-cols-3 gap-4'>
-			<div className='row-span-2'>
+		<div className='grid md:grid-cols-3 gap-4 mx-auto w-full max-w-7xl auto-rows-fr'>
+			<div className='md:row-span-2'>
 				<TechnologyGrid />
 			</div>
-			<ProjectsGrid />
+			<ProjectsItems />
 		</div>
 	);
 };
