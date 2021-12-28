d53f4f759764f64e008f4d48bd1f1938845977b5
swkoyo <10441291+swkoyo@users.noreply.github.com>
2021-12-28 11:50:42 +0900

    build fix

--- src/client/components/Layout.tsx
+++ src/client/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ComponentType } from 'react';
 import Head from 'next/head';
 import Navbar from './Navbar';
+import { FULL_NAME } from '../config';
 import { useUserContext } from '../context/UserContext';
 
 const Layout: ComponentType = ({ children }) => {
@@ -9,7 +10,7 @@ const Layout: ComponentType = ({ children }) => {
 	return (
 		<div className='m-5'>
 			<Head>
-				<title>{process.env.FULL_NAME}</title>
+				<title>{FULL_NAME}</title>
 			</Head>
 			<Navbar />
 			<main className='mt-10 px-6'>{children}</main>
--- src/client/pages/portfolio/index.tsx
+++ src/client/pages/portfolio/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next';
 import ProjectsItems from '../../components/Project/ProjectsItems';
 import TechnologyGrid from '../../components/Technology/TechnologyGrid';
+import { API_URL } from '../../config';
 
 const Portfolio: NextPage = ({
 	projects,
@@ -18,10 +19,10 @@ const Portfolio: NextPage = ({
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-	const projectsRes = await fetch(`${process.env.API_URL}/projects`);
+	const projectsRes = await fetch(`${API_URL}/projects`);
 	const projects = await projectsRes.json();
 
-	const technologiesRes = await fetch(`${process.env.API_URL}/technologies`);
+	const technologiesRes = await fetch(`${API_URL}/technologies`);
 	const technologies = await technologiesRes.json();
 
 	return {
