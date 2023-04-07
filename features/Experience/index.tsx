ae8372c49c7ddede5bb2f23b75326ea4d27cb7cf
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-07 14:33:17 +0900

    projects init

--- features/Experience/index.tsx
+++ features/Experience/index.tsx
@@ -1,5 +1,41 @@
-import { Box } from '@mantine/core';
+import { Group, List, Tabs, Text } from '@mantine/core';
+import { TbArrowBigRightLine } from 'react-icons/tb';
+import Icon from '../../components/Icon';
+import MainContainer from '../../layouts/MainContainer';
+import { JOBS } from './data';
 
 export default function Experience() {
-	return <Box>EXPERIENCE</Box>;
+	return (
+		<MainContainer>
+			<Tabs defaultValue={JOBS[0].title}>
+				<Tabs.List>
+					{JOBS.map(({ title }) => (
+						<Tabs.Tab value={title} key={title}>
+							{title}
+						</Tabs.Tab>
+					))}
+				</Tabs.List>
+				{JOBS.map(({ title, role, dates, tech_stack, tasks }) => (
+					<Tabs.Panel key={title} value={title}>
+						<Group>
+							<Text>{role}</Text>
+							<Text>@{title}</Text>
+						</Group>
+						<Text>{dates}</Text>
+						<List icon={<TbArrowBigRightLine />}>
+							{tasks.map((task) => (
+								<List.Item key={task}>{task}</List.Item>
+							))}
+						</List>
+						{tech_stack.map((tech) => (
+							<Group key={tech}>
+								<Icon type={tech} size='1.5rem' />
+								<Text>{tech}</Text>
+							</Group>
+						))}
+					</Tabs.Panel>
+				))}
+			</Tabs>
+		</MainContainer>
+	);
 }
