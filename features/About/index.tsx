c15125729f48d00fd2f68981b7aa3c4c6cfcc031
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-11 11:47:34 +0900

    Home titles

--- features/About/index.tsx
+++ features/About/index.tsx
@@ -4,36 +4,39 @@ import { BIO, CORE_TECH } from './data';
 
 export default function About() {
 	return (
-		<Grid gutter={50} align='center'>
-			<Grid.Col span={4}>
-				<Avatar
-					src='./avatar.png'
-					size='full'
-					radius={500}
-					sx={(theme) => ({
-						border: `${rem(3)} solid ${theme.colors.dark[4]}`,
-						backgroundColor: 'white'
-					})}
-				/>
-			</Grid.Col>
-			<Grid.Col span={8}>
-				<Stack>
-					{BIO.map((paragraph, i) => (
-						<Text key={i}>{paragraph}</Text>
-					))}
-					<Text>My core tech stack is:</Text>
-					<Grid>
-						{CORE_TECH.map((tech) => (
-							<Grid.Col key={tech} span={4}>
-								<Group>
-									<Icon type={tech} size='1.2rem' />
-									<Text>{tech}</Text>
-								</Group>
-							</Grid.Col>
+		<Stack sx={{ rowGap: 30 }}>
+			<Text size='xl'>About me</Text>
+			<Grid gutter={50} align='center'>
+				<Grid.Col span={4}>
+					<Avatar
+						src='./avatar.png'
+						size='full'
+						radius={500}
+						sx={(theme) => ({
+							border: `${rem(3)} solid ${theme.colors.dark[4]}`,
+							backgroundColor: 'white'
+						})}
+					/>
+				</Grid.Col>
+				<Grid.Col span={8}>
+					<Stack>
+						{BIO.map((paragraph, i) => (
+							<Text key={i}>{paragraph}</Text>
 						))}
-					</Grid>
-				</Stack>
-			</Grid.Col>
-		</Grid>
+						<Text>My core tech stack is:</Text>
+						<Grid>
+							{CORE_TECH.map((tech) => (
+								<Grid.Col key={tech} span={4}>
+									<Group>
+										<Icon type={tech} size='1.2rem' />
+										<Text>{tech}</Text>
+									</Group>
+								</Grid.Col>
+							))}
+						</Grid>
+					</Stack>
+				</Grid.Col>
+			</Grid>
+		</Stack>
 	);
 }
--- features/Experience/index.tsx
+++ features/Experience/index.tsx
@@ -5,56 +5,62 @@ import { JOBS } from './data';
 
 export default function Experience() {
 	return (
-		<Tabs defaultValue={JOBS[0].title} orientation='vertical'>
-			<Tabs.List>
-				{JOBS.map(({ title }) => (
-					<Tabs.Tab value={title} key={title}>
-						{title}
-					</Tabs.Tab>
-				))}
-			</Tabs.List>
-			{JOBS.map(
-				({
-					title,
-					role,
-					start_date,
-					end_date,
-					tech_stack,
-					tasks,
-					url
-				}) => (
-					<Tabs.Panel pl='xl' key={title} value={title}>
-						<Stack>
-							<Stack sx={{ rowGap: 5 }}>
-								<Text>
-									{role}{' '}
-									<Anchor href={url} target='_blank'>
-										@{title}
-									</Anchor>
-								</Text>
-								<Text>
-									{start_date} - {end_date || 'Current'}
-								</Text>
+		<Stack sx={{ rowGap: 30 }}>
+			<Text size='xl'>Where I&apos;ve worked</Text>
+			<Tabs defaultValue={JOBS[0].title} orientation='vertical'>
+				<Tabs.List>
+					{JOBS.map(({ title }) => (
+						<Tabs.Tab value={title} key={title}>
+							{title}
+						</Tabs.Tab>
+					))}
+				</Tabs.List>
+				{JOBS.map(
+					({
+						title,
+						role,
+						start_date,
+						end_date,
+						tech_stack,
+						tasks,
+						url
+					}) => (
+						<Tabs.Panel pl='xl' key={title} value={title}>
+							<Stack>
+								<Stack sx={{ rowGap: 5 }}>
+									<Text>
+										{role}{' '}
+										<Anchor href={url} target='_blank'>
+											@{title}
+										</Anchor>
+									</Text>
+									<Text>
+										{start_date} - {end_date || 'Current'}
+									</Text>
+								</Stack>
+								<Grid>
+									{tech_stack.map((tech) => (
+										<Grid.Col span={3} key={tech}>
+											<Group spacing='xs'>
+												<Icon
+													type={tech}
+													size='1.2rem'
+												/>
+												<Text size='sm'>{tech}</Text>
+											</Group>
+										</Grid.Col>
+									))}
+								</Grid>
+								<List icon={<TbArrowBigRightLine />}>
+									{tasks.map((task) => (
+										<List.Item key={task}>{task}</List.Item>
+									))}
+								</List>
 							</Stack>
-							<Grid>
-								{tech_stack.map((tech) => (
-									<Grid.Col span={3} key={tech}>
-										<Group spacing='xs'>
-											<Icon type={tech} size='1.2rem' />
-											<Text size='sm'>{tech}</Text>
-										</Group>
-									</Grid.Col>
-								))}
-							</Grid>
-							<List icon={<TbArrowBigRightLine />}>
-								{tasks.map((task) => (
-									<List.Item key={task}>{task}</List.Item>
-								))}
-							</List>
-						</Stack>
-					</Tabs.Panel>
-				)
-			)}
-		</Tabs>
+						</Tabs.Panel>
+					)
+				)}
+			</Tabs>
+		</Stack>
 	);
 }
