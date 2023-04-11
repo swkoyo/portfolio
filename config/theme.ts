1377778a110d4b60f5e9172674c3de1d4bdf9935
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-11 10:38:59 +0900

    add brand color shceme

--- config/theme.ts
+++ config/theme.ts
@@ -3,7 +3,22 @@ import '@fontsource/montserrat';
 
 const theme: MantineThemeOverride = {
 	colorScheme: 'dark',
-	fontFamily: 'Montserrat, sans-serif'
+	fontFamily: 'Montserrat, sans-serif',
+	colors: {
+		brand: [
+			'#edf7f7',
+			'#cae7e6',
+			'#a7d7d6',
+			'#84c7c5',
+			'#61b8b5',
+			'#479e9b',
+			'#387b79',
+			'#285856',
+			'#183534',
+			'#081211'
+		]
+	},
+	primaryColor: 'brand'
 };
 
 export default theme;
