75a48f254c2ca9444eda44603f41c4313862f1e3
swkoyo <10441291+swkoyo@users.noreply.github.com>
2023-04-14 12:45:15 +0900

    loading init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { Center, Loader, Stack, rem } from '@mantine/core';
-import { useTimeout } from '@mantine/hooks';
+import { Center, RingProgress, rem } from '@mantine/core';
+import { useInterval, useTimeout } from '@mantine/hooks';
 import type { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { TbCheck } from 'react-icons/tb';
 import MainLogo from '../components/MainLogo';
 import { NODE_ENV } from '../constants';
 import MainShell from '../layouts/MainShell';
@@ -10,15 +11,45 @@ const Main: NextPage = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(
 		NODE_ENV === 'production'
 	);
-	useTimeout(() => setIsLoading(false), 2000, { autoInvoke: true });
+	const [seconds, setSeconds] = useState(0);
+	const interval = useInterval(() => setSeconds((s) => s + 1), 10);
+	const { start, clear } = useTimeout(() => {
+		setIsLoading(false);
+		clear();
+	}, 1000);
+
+	useEffect(() => {
+		interval.start();
+	}, []);
+
+	useEffect(() => {
+		if (interval.active && seconds >= 100) {
+			interval.stop();
+			start();
+		}
+	}, [seconds, interval, start]);
 
 	if (isLoading) {
 		return (
 			<Center h='100vh'>
-				<Stack align='center'>
-					<MainLogo width={rem(100)} />
-					<Loader />
-				</Stack>
+				<RingProgress
+					size={200}
+					sections={[{ value: seconds, color: 'teal' }]}
+					sx={{
+						transform: seconds < 100 ? undefined : 'scale(2)',
+						transition: 'all .3s ease-in-out'
+					}}
+					roundCaps
+					label={
+						<Center>
+							{seconds < 100 ? (
+								<MainLogo width={rem(70)} />
+							) : (
+								<TbCheck color='teal' size={rem(70)} />
+							)}
+						</Center>
+					}
+				/>
 			</Center>
 		);
 	}
